f6c70a357ea76d3aed26fda5f20a9f8c7828dc62
singuerinc <nahuel.scotti@gmail.com>
2018-05-17 23:22:11 +0200

    update tests

--- randToWord.test.js
+++ randToWord.test.js
@@ -3,7 +3,7 @@ const { randToWord } = require('./randToWord');
 var assert = require('assert');
 
 describe('randToWord', function() {
-  describe('randToWord', function() {
+  describe('simple', function() {
     it('should return 00000', function() {
       assert.equal(randToWord('ABCDE', 0, 0), '00000');
     });
@@ -31,24 +31,25 @@ describe('randToWord', function() {
     it('should return ABCD1', function() {
       assert.equal(randToWord('ABCD1', 4, 1), 'ABCD1');
     });
-
+  });
+  describe('with spaces', function() {
     it('should return Yugo Nakamura', function() {
       assert.equal(randToWord('Yugo Nakamura', 13, 0), 'Yugo Nakamura');
     });
 
-    it('should return spaces as spaces', function() {
+    it('should return with spaces 1', function() {
       assert.equal(randToWord('Yugo Nakamura', 3, 0), 'Yug0 00000000');
     });
 
-    it('should return spaces as spaces', function() {
+    it('should return with spaces 2', function() {
       assert.equal(randToWord('Yugo Nakamura', 4, 0), 'Yugo 00000000');
     });
 
-    it('should return spaces as spaces', function() {
+    it('should return with spaces 3', function() {
       assert.equal(randToWord('Yugo Nakamura', 5, 0), 'Yugo 00000000');
     });
 
-    it('should return spaces as spaces', function() {
+    it('should return with spaces 4', function() {
       assert.equal(randToWord('Yugo Nakamura', 6, 0), 'Yugo N0000000');
     });
   });
